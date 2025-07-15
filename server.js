4f68ab42fbb34568ac395007c16fd8351e363730
KodeToReality <smimynah@gmail.com>
2025-07-15 08:14:05 -0400

    Add Fastify server with MongoDB and routes
    Initial commit with Fastify server setup, MongoDB connector plugin, and basic routes for handling animal data. Includes package files for dependencies.

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// ESM
+import Fastify from 'fastify'
+import dbConnector from './our-db-connector.js'
+import firstRoute from './our-first-route.js'
+
+/**
+ * @type {import('fastify').FastifyInstance} Instance of Fastify
+ */
+const fastify = Fastify({
+  logger: true
+})
+fastify.register(dbConnector)
+fastify.register(firstRoute)
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  // Server is now listening on ${address}
+})
